7bd2cc2081f9173eb7d83dc58e52d0351f01c5f7
OccassionalDev <ericaschimmel01@gmail.com>
2020-12-07 22:02:58 -0500

    added validation to validate a valid name in User model

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,5 +4,12 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
+  validate :valid_name
   validates :name, presence: true, length: { maximum: 30, minimum: 2 }
+
+  private 
+
+  def valid_name
+    errors.add(:name, "must only contain letters and spaces") if name =~ /[^a-zA-Z\s]/
+  end 
 end
