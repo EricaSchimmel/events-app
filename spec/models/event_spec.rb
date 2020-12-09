a14edf6eaa7c12fb0711c344266c799e53b1a63d
OccassionalDev <ericaschimmel01@gmail.com>
2020-12-08 22:20:16 -0500

    refactored User model tests, and added more validity tests to Event model tests

--- spec/models/event_spec.rb
+++ spec/models/event_spec.rb
@@ -12,4 +12,16 @@ RSpec.describe Event, type: :model do
 
     expect(event2).to_not be_valid
   end 
+
+  it "is not valid without a description" do 
+    event2 = build(:event, description: nil)
+
+    expect(event2).to_not be_valid
+  end 
+
+  it "is not valid without a date" do 
+    event2 = build(:event, date: nil)
+
+    expect(event2).to_not be_valid
+  end 
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -2,55 +2,58 @@ require 'rails_helper'
 require 'faker'
 
 RSpec.describe User, type: :model do
-  before(:all) do 
-    @user1 = create(:user)
+  before(:each) do 
+    @user = build(:user)
   end 
 
   it "is valid with valid attributes" do 
-    expect(@user1).to be_valid 
+    expect(@user).to be_valid 
   end
 
   it "is not valid with a invalid email" do 
-    user2 = build(:user, email: "test @example.com")
+    @user.email = "test @example.com"
 
-    expect(user2).to_not be_valid 
+    expect(@user).to_not be_valid 
   end 
 
   it "is not valid when passwords do not match" do 
-    user2 = build(:user, email: Faker::Internet.email, password_confirmation: "sample")
+    @user.password_confirmation = "asdf"
 
-    expect(user2).to_not be_valid
+    expect(@user).to_not be_valid
   end 
 
   it "is not valid without an email" do 
-    user2 = build(:user, email: nil)
+    @user.email = nil
 
-    expect(user2).to_not be_valid
+    expect(@user).to_not be_valid
   end 
 
   it "is not valid without a password" do 
-    user2 = build(:user, email: Faker::Internet.email, password: nil)
+    @user.password = nil
 
-    expect(user2).to_not be_valid 
+    expect(@user).to_not be_valid 
   end 
 
   it "is not valid without a name" do 
-    user2 = build(:user, name: nil, email: Faker::Internet.email, name: nil)
+    @user.name = nil
 
-    expect(user2).to_not be_valid
+    expect(@user).to_not be_valid
   end 
 
   it "is valid with a valid name" do 
-    user2 = build(:user, email: Faker::Internet.email, name: Faker::Name.first_name)
-
-    expect(user2).to be_valid
+    expect(@user).to be_valid
   end 
 
   it "is not valid with a invalid name" do 
-    user2 = build(:user, email: Faker::Internet.email, name: "a")
-    user3 = build(:user, email: Faker::Internet.email, name: "1Jack")
+    @user.name = "1Jack"
 
-    expect(user2).to_not be_valid 
-    expect(user3).to_not be_valid 
+    expect(@user).to_not be_valid 
+  end 
+
+  it "is not valid with a existing email" do
+    user2 = create(:user, email: Faker::Internet.email)
+    @user.email = user2.email
+
+    expect(@user).to_not be_valid
   end 
 end
