078f5ccfa3ff46e8dc6264a2eb9c9942021e6359
OccassionalDev <ericaschimmel01@gmail.com>
2020-12-10 23:24:11 -0500

    added association tests and seperated them from validation tests for the User model tests

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -5,49 +5,55 @@ RSpec.describe User, type: :model do
   before(:each) do 
     @user = build(:user)
   end 
+  describe "validations" do
+    it "is valid with valid attributes" do 
+      expect(@user).to be_valid 
+    end
 
-  it "is valid with valid attributes" do 
-    expect(@user).to be_valid 
-  end
+    it "is not valid with a invalid email" do 
+      @user.email = "test @example.com"
+      expect(@user).to_not be_valid 
+    end 
 
-  it "is not valid with a invalid email" do 
-    @user.email = "test @example.com"
-    expect(@user).to_not be_valid 
+    it "is not valid when passwords do not match" do 
+      @user.password_confirmation = "asdf"
+      expect(@user).to_not be_valid
+    end 
+
+    it "is not valid without an email" do 
+      @user.email = nil
+      expect(@user).to_not be_valid
+    end 
+
+    it "is not valid without a password" do 
+      @user.password = nil
+      expect(@user).to_not be_valid 
+    end 
+
+    it "is not valid without a name" do 
+      @user.name = nil
+      expect(@user).to_not be_valid
+    end 
+
+    it "is valid with a valid name" do 
+      expect(@user).to be_valid
+    end 
+
+    it "is not valid with a invalid name" do 
+      @user.name = "1Jack"
+      expect(@user).to_not be_valid 
+    end 
+
+    it "is not valid with a existing email" do
+      user2 = create(:user, email: Faker::Internet.email)
+      @user.email = user2.email
+
+      expect(@user).to_not be_valid
+    end 
   end 
 
-  it "is not valid when passwords do not match" do 
-    @user.password_confirmation = "asdf"
-    expect(@user).to_not be_valid
-  end 
-
-  it "is not valid without an email" do 
-    @user.email = nil
-    expect(@user).to_not be_valid
-  end 
-
-  it "is not valid without a password" do 
-    @user.password = nil
-    expect(@user).to_not be_valid 
-  end 
-
-  it "is not valid without a name" do 
-    @user.name = nil
-    expect(@user).to_not be_valid
-  end 
-
-  it "is valid with a valid name" do 
-    expect(@user).to be_valid
-  end 
-
-  it "is not valid with a invalid name" do 
-    @user.name = "1Jack"
-    expect(@user).to_not be_valid 
-  end 
-
-  it "is not valid with a existing email" do
-    user2 = create(:user, email: Faker::Internet.email)
-    @user.email = user2.email
-
-    expect(@user).to_not be_valid
+  describe "associations" do 
+    it { should have_many(:hosted_events).class_name("Event") }
+    it { should have_many(:reserved_events) }
   end 
 end
