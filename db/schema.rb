25d08b6677815f4a97e0a9123ded6063aa58782d
OccassionalDev <ericaschimmel01@gmail.com>
2020-12-14 23:52:14 -0500

    migrated database

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_12_09_021133) do
+ActiveRecord::Schema.define(version: 2020_12_15_045017) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -29,6 +29,14 @@ ActiveRecord::Schema.define(version: 2020_12_09_021133) do
     t.integer "host_id"
   end
 
+  create_table "reservations", force: :cascade do |t|
+    t.integer "guest_id"
+    t.bigint "event_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["event_id"], name: "index_reservations_on_event_id"
+  end
+
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
@@ -42,4 +50,5 @@ ActiveRecord::Schema.define(version: 2020_12_09_021133) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "reservations", "events"
 end
