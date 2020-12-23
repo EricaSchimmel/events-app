af5d50618f538686eafc96e744a58c51d76587dd
OccassionalDev <ericaschimmel01@gmail.com>
2020-12-23 17:57:00 -0500

    added CRUD actions to Events controller, created event_params method and a find_event before action in Events controller

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,2 +1,28 @@
 class EventsController < ApplicationController
+    before_action :find_event, only: [:show, :edit, :update, :destroy]
+
+    def new
+    end 
+    
+    def create
+    end 
+
+    def edit
+    end 
+
+    def update
+    end 
+
+    def destroy
+    end 
+
+    private 
+
+    def event_params
+        params.require(:event).permit(:title, :date, :description, :online, :online_link, :attendee_limit, :start_time, :end_time)
+    end 
+
+    def find_event
+        @event = Event.find(params[:id])
+    end 
 end
