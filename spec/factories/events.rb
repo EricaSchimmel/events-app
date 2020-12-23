c298890eb0359761372c4f4470f0948e14ae33c3
OccassionalDev <ericaschimmel01@gmail.com>
2020-12-23 01:57:16 -0500

    added use of faker for date on Event factory

--- spec/factories/events.rb
+++ spec/factories/events.rb
@@ -4,7 +4,7 @@ FactoryBot.define do
     title { Faker::Lorem.sentence }
     description { Faker::Lorem.paragraph }
     online { true }
-    date { Date.today }
+    date { Faker::Date.between( from: Date.today, to: 7.days.from_now) }
     start_time { "14:00" }
     end_time { "15:00" }
     online_link { Faker::Internet.url }
