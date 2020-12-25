94077cf5640809e8a1f0865832e9978e2fa0a34e
OccassionalDev <ericaschimmel01@gmail.com>
2020-12-24 20:28:57 -0500

    removed validation test from Reservations model tests

--- spec/models/reservation_spec.rb
+++ spec/models/reservation_spec.rb
@@ -1,17 +1,6 @@
 require 'rails_helper'
 
 RSpec.describe Reservation, type: :model do
-  subject { build(:reservation) }
-
-  describe "validations" do 
-    it "should be valid when feedback is not given" do 
-      subject.review = nil 
-      expect(subject).to be_valid
-    end 
-  end 
-
-  describe "associations" do 
-    it { should belong_to(:user) }
-    it { should belong_to(:event) }
-  end 
+  it { should belong_to(:user) }
+  it { should belong_to(:event) }
 end
