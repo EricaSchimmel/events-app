78f0512113464a8175954a6972cffa4635dd6dde
OccassionalDev <ericaschimmel01@gmail.com>
2020-12-29 00:22:37 -0500

    added data to the seed file to be generated

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,23 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+3.times do 
+    User.create(
+        name: Faker::Name.name[2..30],
+        email: Faker::Internet.email,
+        password: "password",
+        password_confirmation: "password"
+    )
+end 
+
+user1 = User.first 
+user2 = User.second 
+user3 = User.third
+
+user1.hosted_events.create( 
+    title: Faker::Lorem.sentence,
+    description: Faker::Lorem.paragraph,
+    online: true,
+    date: Faker::Date.between( from: Date.today, to: 7.days.from_now),
+    start_time: "14:00",
+    end_time: "15:00",
+    online_link: Faker::Internet.url,
+    attendee_limit: 15
+)
