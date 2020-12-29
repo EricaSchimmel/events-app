c521363c0581a06d7731890267924e05bcaf52af
OccassionalDev <ericaschimmel01@gmail.com>
2020-12-28 23:39:00 -0500

    modified routes to have Reservations nested in Events

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,11 @@
 Rails.application.routes.draw do
-  resources :reservations
-  resources :events
-  devise_for :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
+  devise_for :users
+
+  resources :events do 
+    resources :reservations, only: [:index, :create]
+  end 
+
+  resources :reservations, only: [:destroy]
 end
