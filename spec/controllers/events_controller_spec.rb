d88c77bbd4ed310cc3b9aa315f21e6d895d20877
OccassionalDev <ericaschimmel01@gmail.com>
2020-12-28 23:34:20 -0500

    added described each action in Events and Reservations controller tests

--- spec/controllers/events_controller_spec.rb
+++ spec/controllers/events_controller_spec.rb
@@ -1,4 +1,18 @@
 require 'rails_helper'
 
 RSpec.describe EventsController, type: :controller do 
+    describe "GET #new" do 
+    end 
+
+    describe "PUT #create" do
+    end 
+
+    describe "GET #edit" do 
+    end 
+
+    describe "PUT #update" do 
+    end 
+
+    describe "DELETE #destroy" do 
+    end 
 end 
--- spec/controllers/reservations_controller_spec.rb
+++ spec/controllers/reservations_controller_spec.rb
@@ -1,4 +1,9 @@
 require 'rails_helper'
 
 RSpec.describe ReservationsController, type: :controller do
+    desribe "PUT #create" do 
+    end 
+
+    describe "DELETE #destroy" do
+    end 
 end 
