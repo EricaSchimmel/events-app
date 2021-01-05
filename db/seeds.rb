6f36518a59ec1dae063b9cf20033685c03af1168
OccassionalDev <ericaschimmel01@gmail.com>
2021-01-04 22:05:14 -0500

    removed start_time from seed file on event generation

--- db/seeds.rb
+++ db/seeds.rb
@@ -16,7 +16,6 @@ user1.hosted_events.create(
     description: Faker::Lorem.paragraph,
     online: true,
     start_date: Faker::Date.between( from: Date.today, to: 7.days.from_now),
-    start_time: "14:00",
     end_time: "15:00",
     online_link: Faker::Internet.url,
     attendee_limit: 15
