f875b2f981e8ab6145beebe6bc8b2480e7a24662
OccassionalDev <ericaschimmel01@gmail.com>
2021-01-04 22:15:02 -0500

    removed end_date column and added it back in with a datetime format in Events

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_01_05_030541) do
+ActiveRecord::Schema.define(version: 2021_01_05_031323) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -20,12 +20,12 @@ ActiveRecord::Schema.define(version: 2021_01_05_030541) do
     t.string "description"
     t.boolean "online"
     t.datetime "start_date"
-    t.string "end_date"
     t.string "online_link"
     t.integer "attendee_limit"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.integer "host_id"
+    t.datetime "end_date"
   end
 
   create_table "reservations", force: :cascade do |t|
