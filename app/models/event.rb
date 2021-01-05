a89f2632005aad735b282b4ca5ad6d6d0b3d0922
OccassionalDev <ericaschimmel01@gmail.com>
2021-01-04 22:48:04 -0500

    changed end_date validation to be valid for a datetime after the start_date in Event model

--- app/models/event.rb
+++ app/models/event.rb
@@ -6,7 +6,7 @@ class Event < ApplicationRecord
   validates :online, :exclusion => [nil]
 
   validates_datetime :start_date, on_or_after: :today
-  validates_datetime :end_date, on_or_after: :start_date
+  validates_datetime :end_date, after: :start_date
 
   validates :online_link, :presence => true, :format => { :with => URI.regexp }, :if => :event_is_online?
   validate :attendee_limit_reached?, :on => :update, :unless => -> { attendee_limit.nil? }
