4168d89ff16f169a88b23a2ccec87486177a97f6
OccassionalDev <ericaschimmel01@gmail.com>
2021-01-06 18:27:03 -0500

    changed code syntax for symbols in event generation in seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -15,8 +15,8 @@ user1.hosted_events.create(
     title: Faker::Lorem.sentence,
     description: Faker::Lorem.paragraph,
     online: true,
-    start_date: Faker::Time.between(from: DateTime.now, to: DateTime.now + 1),
-    end_date: Faker::Time.between(from: DateTime.now + 2, to: DateTime.now + 3),
+    start_date: Faker::Time.between(:from => DateTime.now, :to => DateTime.now + 1),
+    end_date: Faker::Time.between(:from => DateTime.now + 2, :to => DateTime.now + 3),
     online_link: Faker::Internet.url,
     attendee_limit: 15
 )
