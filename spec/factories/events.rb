58e29ac72c7c04df6f1b7c509922337c58426ad9
OccassionalDev <ericaschimmel01@gmail.com>
2021-01-06 18:19:46 -0500

    rubocop format fixes

--- spec/factories/events.rb
+++ spec/factories/events.rb
@@ -4,8 +4,8 @@ FactoryBot.define do
     title { Faker::Lorem.sentence }
     description { Faker::Lorem.paragraph }
     online { true }
-    start_date { Faker::Time.between(from: DateTime.now, to: DateTime.now + 1) }
-    end_date { Faker::Time.between(from: DateTime.now + 2, to: DateTime.now + 3) }
+    start_date { Faker::Time.between(:from => DateTime.now, :to => DateTime.now + 1) }
+    end_date { Faker::Time.between(:from => DateTime.now + 2, :to => DateTime.now + 3) }
     online_link { Faker::Internet.url }
     attendee_limit { 15 }
   end
