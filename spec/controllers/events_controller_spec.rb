17b4b9af10ec0cb1bc1d6267ba6b17a1d97098fe
OccassionalDev <ericaschimmel01@gmail.com>
2021-01-08 21:42:39 -0500

    added pending messages to Events and Reservations controller and request tests

--- spec/controllers/events_controller_spec.rb
+++ spec/controllers/events_controller_spec.rb
@@ -2,17 +2,22 @@ require 'rails_helper'
 
 RSpec.describe EventsController, :type => :controller do
     describe 'GET #new' do
+        pending 'Renders the new template'
     end
 
     describe 'PUT #create' do
+        pending 'Creates a new event succesfully'
     end
 
     describe 'GET #edit' do
+        pending 'Renders the edit template'
     end
 
     describe 'PUT #update' do
+        pending 'Updates a event with new valid data succesfully'
     end
 
     describe 'DELETE #destroy' do
+        pending 'Deletes the event'
     end
 end
