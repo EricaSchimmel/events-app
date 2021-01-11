013163d302a223ac8795d2b4442fb3ee83426b5a
OccassionalDev <ericaschimmel01@gmail.com>
2021-01-11 15:53:40 -0500

    Reservations model tests now include the counter cache, updated Events model with start_date valiadtion to use DateTime.now

--- spec/models/reservation_spec.rb
+++ spec/models/reservation_spec.rb
@@ -2,5 +2,5 @@ require 'rails_helper'
 
 RSpec.describe Reservation, :type => :model do
   it { should belong_to(:user) }
-  it { should belong_to(:event) }
+  it { should belong_to(:event).counter_cache(:guests_count) }
 end
