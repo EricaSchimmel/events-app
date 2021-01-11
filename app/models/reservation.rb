b36f54699303e6030d22b05ad0e0f979e8184dc9
OccassionalDev <ericaschimmel01@gmail.com>
2021-01-11 15:33:49 -0500

    added guests counter cache to Events

--- app/models/reservation.rb
+++ app/models/reservation.rb
@@ -1,4 +1,4 @@
 class Reservation < ApplicationRecord
-  belongs_to :event
+  belongs_to :event, :counter_cache => :guests_count
   belongs_to :user
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_01_05_031323) do
+ActiveRecord::Schema.define(version: 2021_01_09_024543) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -26,6 +26,7 @@ ActiveRecord::Schema.define(version: 2021_01_05_031323) do
     t.datetime "updated_at", precision: 6, null: false
     t.integer "host_id"
     t.datetime "end_date"
+    t.integer "guests_count", default: 0, null: false
   end
 
   create_table "reservations", force: :cascade do |t|
