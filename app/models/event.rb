3cda2f3eaf6cd95bef0b985568ab6123c44f4fe9
OccassionalDev <ericaschimmel01@gmail.com>
2021-01-11 15:54:55 -0500

    rubocop fix

--- app/models/event.rb
+++ app/models/event.rb
@@ -5,7 +5,7 @@ class Event < ApplicationRecord
   validates_presence_of :title, :description, :start_date, :end_date
   validates :online, :exclusion => [nil]
 
-  validates_datetime :start_date, :on_or_after => lambda { DateTime.now }
+  validates_datetime :start_date, :on_or_after => -> { DateTime.now }
   validates_datetime :end_date, :after => :start_date
 
   validates :online_link, :presence => true, :format => { :with => URI.regexp }, :if => :event_is_online?
