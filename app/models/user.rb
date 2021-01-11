014969bf42a120255c69e378ff14a6021e8b7bff
OccassionalDev <ericaschimmel01@gmail.com>
2021-01-11 15:42:07 -0500

    updated name format validation for Users to allow apostrophes

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,7 +5,7 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
   validates :name, :presence => true, :length => { :maximum => 30, :minimum => 2 },
-                   :format => { :without => /[^a-zA-Z\s.]/, :message => 'cannot contain numbers' }
+                   :format => { :without => /[^a-zA-Z\s.']/, :message => 'must only contain letters, periods, and apostrophes.' }
 
   has_many :hosted_events, :class_name => 'Event', :foreign_key => :host_id
   has_many :reservations
