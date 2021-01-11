50aed31fa0e09a4bd651b18e9cbc3f19d0db71f4
OccassionalDev <ericaschimmel01@gmail.com>
2021-01-11 15:35:03 -0500

    rubocop indentation fixes

--- spec/requests/events_request_spec.rb
+++ spec/requests/events_request_spec.rb
@@ -1,5 +1,5 @@
 require 'rails_helper'
 
 RSpec.describe "Events", :type => :request do
-    pending 'Will add some tests when a JSON response is needed...'
+  pending 'Will add some tests when a JSON response is needed...'
 end
--- spec/requests/reservations_request_spec.rb
+++ spec/requests/reservations_request_spec.rb
@@ -1,5 +1,5 @@
 require 'rails_helper'
 
 RSpec.describe "Reservations", :type => :request do
-    pending 'Will add some tests when a JSON response is needed...'
+  pending 'Will add some tests when a JSON response is needed...'
 end
