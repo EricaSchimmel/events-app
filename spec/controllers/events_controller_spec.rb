b91b63ed373efc08a2d4a9717e5cb85259b9fa3e
OccassionalDev <ericaschimmel01@gmail.com>
2021-01-12 23:04:50 -0500

    Events controller tests check for the use of authenticate_user! before action

--- spec/controllers/events_controller_spec.rb
+++ spec/controllers/events_controller_spec.rb
@@ -2,10 +2,14 @@ require 'rails_helper'
 
 RSpec.describe EventsController, :type => :controller do
     describe 'GET #new' do
+        it { should use_before_action(:authenticate_user!) }
+
         pending 'Renders the new template'
     end
 
     describe 'PUT #create' do
+        it { should use_before_action(:authenticate_user!) }
+
         pending 'Creates a new event succesfully'
     end
 
@@ -16,16 +20,20 @@ RSpec.describe EventsController, :type => :controller do
     end
 
     describe 'GET #edit' do
+        it { should use_before_action(:authenticate_user!) }
         it { should use_before_action(:set_event) }
 
         pending 'Renders the edit template'
     end
 
     describe 'PUT #update' do
+        it { should use_before_action(:authenticate_user!) }
+
         pending 'Updates a event with new valid data succesfully'
     end
 
     describe 'DELETE #destroy' do
+        it { should use_before_action(:authenticate_user!) }
         it { should use_before_action(:set_event) }
 
         pending 'Deletes the event'
