b6db905b99de5d3ddc1ba02bc24c0c69a4f560bb
OccassionalDev <ericaschimmel01@gmail.com>
2021-01-21 23:37:30 -0500

    added and refactored validate_quota validation for Reservations to prevent unwanted reservations that go over a Event's guest limit

--- app/models/reservation.rb
+++ app/models/reservation.rb
@@ -2,13 +2,13 @@ class Reservation < ApplicationRecord
   belongs_to :event, :counter_cache => :guests_count
   belongs_to :user
 
-  # validate :validate_quota, :on => :create
+  validate :validate_quota, :on => :create
 
-  # def validate_quota
-  #   return unless !self.event.guest_limit.nil? && self.event.guest_limit != 0
+  def validate_quota
+    return unless !self.event.guest_limit.nil? && self.event.guest_limit != 0
 
-  #   if self.event.guests(:reload).size > self.event.guest_limit
-  #     errors.add(:base, 'Guest limit reached')
-  #   end
-  # end
+    if self.event.guests.reload.size >= self.event.guest_limit
+      errors.add(:base, 'guest limit reached')
+    end
+  end
 end
