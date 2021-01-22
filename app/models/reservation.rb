5c42d645a61632ef8fe3dee41919bac795c6057c
OccassionalDev <ericaschimmel01@gmail.com>
2021-01-22 17:38:12 -0500

    added validation to prevent the same user from reserving twice to the same event in Reservations model

--- app/models/reservation.rb
+++ app/models/reservation.rb
@@ -3,10 +3,17 @@ class Reservation < ApplicationRecord
   belongs_to :user
 
   validate :validate_quota, :on => :create
+  validate :validate_existing, :on => :create
 
   def validate_quota
     return unless event && event.guest_limit != 0
 
     errors.add(:base, 'guest limit reached') if event.guests.reload.size >= event.guest_limit
+  end
+
+  def validate_existing
+    return unless event && user
+
+    errors.add(:base, 'reservation already created') if !event.guests.find_by(user_id: user.id).nil?
   end
 end
