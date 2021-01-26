0fa62417347b037edca52df52c255a1ba4ab71f7
OccassionalDev <ericaschimmel01@gmail.com>
2021-01-25 22:13:31 -0500

    added pending tests in Reservations Request spec

--- spec/requests/reservations_request_spec.rb
+++ spec/requests/reservations_request_spec.rb
@@ -1,5 +1,19 @@
 require 'rails_helper'
 
 RSpec.describe "Reservations", :type => :request do
-  pending 'Will add some tests when a JSON response is needed...'
+  describe 'GET #index' do
+    pending 'it assigns a list of all reservations to @reservations'
+    pending 'it has a successful status'
+    pending 'it returns an errors status with for a non-exiting event'
+  end
+
+  describe 'POST #create' do
+    pending 'it creates a new reservation'
+    pending 'it returns an errors status with for a non-exiting event'
+  end
+
+  describe 'DELETE #destroy' do
+    pending 'it deletes a existing reservation'
+    pending 'it returns an error status with a non-existing reservation'
+  end
 end
