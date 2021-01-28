beeee51569fc567b03899ceb8fc76d01f62b474f
OccassionalDev <ericaschimmel01@gmail.com>
2021-01-27 21:34:19 -0500

    refactored Reservations request spec for #create and #index tests

--- spec/requests/reservations_request_spec.rb
+++ spec/requests/reservations_request_spec.rb
@@ -3,24 +3,22 @@ require 'rails_helper'
 RSpec.describe 'Reservations', :type => :request do
   describe 'GET #index' do
     context 'a parameter with an existing event is specified' do
+      let!(:event) { create(:event) }
+      before(:each) { get event_reservations_path, :params => { :event_id => event.id } }
+
       pending 'it assigns a list of all reservations to @reservations' do
-        event = create(:event)
         create(:reservation, event_id: event.id)
-
-        # get request here
-
         expect(assigns(:reservations)).to eq(event.guests)
       end
 
       pending 'it has a successful status' do
-        # get request here
         expect(response).to have_http_status(:successful)
       end
     end
 
     context 'a non-existing event is specified' do
       pending 'it returns an errors status with for a non-exiting event' do
-        # get request here
+        get event_reservations_path, :params => { :event_id => 0 }
         expect(reponse).to have_http_status(:error)
       end
     end
@@ -28,10 +26,6 @@ RSpec.describe 'Reservations', :type => :request do
 
   describe 'POST #create' do
     pending 'it creates a new reservation'
-
-    pending 'it returns an errors status with for a non-exiting event' do
-      expect(reponse).to have_http_status(:error)
-    end
   end
 
   describe 'DELETE #destroy' do
