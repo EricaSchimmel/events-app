c96553216f597f2128bf05d163ed6c0a4982ebfe
OccassionalDev <ericaschimmel01@gmail.com>
2021-02-07 00:37:24 -0500

    added functionality to index action in Reservations Controller

--- app/controllers/reservations_controller.rb
+++ app/controllers/reservations_controller.rb
@@ -2,6 +2,7 @@ class ReservationsController < ApplicationController
   before_action :authenticate_user!, :except => [:index]
 
   def index
+    @pagy, @reservations = pagy(Reservation.where(:event_id => params[:event_id]))
   end
 
   def create
