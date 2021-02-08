73ad9522a2dd71c4c5c5fc500a98e7881ec0223a
OccassionalDev <ericaschimmel01@gmail.com>
2021-02-08 18:18:12 -0500

    added functionality to create and destroy actions of Reservations Controller, created UsersController with a functional show action

--- app/controllers/reservations_controller.rb
+++ app/controllers/reservations_controller.rb
@@ -6,8 +6,17 @@ class ReservationsController < ApplicationController
   end
 
   def create
+    @reservation = current_user.reservations.build(:event_id => params[:event_id])
+
+    if @reservation.save?
+      redirect_to event_path(params[:event_id])
+    else
+      redirect_to event_path(params[:event_id]),
+                  :alert => "Reservation could not be created because #{@reservation.errors.full_messages}"
+    end
   end
 
   def destroy
+    @reservation = Reservation.find(params[:reservation_id])
   end
 end
