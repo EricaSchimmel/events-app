fb14d33e9e0338515099f8b672c631522332b349
OccassionalDev <ericaschimmel01@gmail.com>
2021-02-09 21:53:17 -0500

    added validation on Reservations to check to see if an event host is making a reservation to their own event

--- app/models/reservation.rb
+++ app/models/reservation.rb
@@ -4,6 +4,7 @@ class Reservation < ApplicationRecord
 
   validate :validate_quota, :on => :create
   validate :validate_existing, :on => :create
+  validate :validate_event_host, :on => :create
 
   def validate_quota
     return unless event && event.guest_limit != 0
@@ -15,5 +16,11 @@ class Reservation < ApplicationRecord
     return unless event && user
 
     errors.add(:base, 'reservation already created') unless event.guests.find_by(:user_id => user.id).nil?
+  end
+
+  def validate_event_host
+    return unless event && user
+
+    errors.add(:base, 'event hosts do not need a reservation') if user.id == event.host_id
   end
 end
