dda7a4f152f439c5e38bf5c37d44b5adcf7ed089
OccassionalDev <ericaschimmel01@gmail.com>
2021-02-09 22:02:54 -0500

    added more validations to Reservations to validate if a reservation can be made if the event has ended or started

--- app/models/reservation.rb
+++ app/models/reservation.rb
@@ -5,6 +5,8 @@ class Reservation < ApplicationRecord
   validate :validate_quota, :on => :create
   validate :validate_existing, :on => :create
   validate :validate_event_host, :on => :create
+  validate :validate_if_event_ended, :on => :create
+  validate :validate_if_event_has_already_started, :on => :create
 
   def validate_quota
     return unless event && event.guest_limit != 0
@@ -22,5 +24,17 @@ class Reservation < ApplicationRecord
     return unless event && user
 
     errors.add(:base, 'event hosts do not need a reservation') if user.id == event.host_id
+  end
+
+  def validate_if_event_ended
+    return unless event && user
+
+    errors.add(:base, 'event has already ended') if DateTime.now >= event.end_date
+  end
+
+  def validate_if_event_has_already_started
+    return unless event && user
+
+    errors.add(:base, 'event has already started') if DateTime.now >= event.start_date
   end
 end
