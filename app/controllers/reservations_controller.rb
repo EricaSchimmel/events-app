e9c941bdf772266eb81086054c354ca119d979a5
OccassionalDev <ericaschimmel01@gmail.com>
2021-03-08 16:58:15 -0500

    added before_action check_if_event_exists_and_set_event to Reservations controller to check if an event exists, changed mentions of params[:event_id] to @event.id in Reservations controller where needed

--- app/controllers/reservations_controller.rb
+++ app/controllers/reservations_controller.rb
@@ -1,9 +1,10 @@
 class ReservationsController < ApplicationController
   before_action :authenticate_user!, :except => [:index]
+  before_action :check_if_event_exists_and_set_event, :except => [:destroy]
 
   def index
     if params[:event_id]
-      @pagy, @reservations = pagy(Reservation.where(:event_id => params[:event_id]))
+      @pagy, @reservations = pagy(Reservation.where(:event_id => @event.id))
 
     elsif params[:user_id]
       authenticate_user!
@@ -14,12 +15,12 @@ class ReservationsController < ApplicationController
   end
 
   def create
-    @reservation = current_user.reservations.build(:event_id => params[:event_id])
+    @reservation = current_user.reservations.build(:event_id => @event.id)
 
     if @reservation.save?
-      redirect_to event_path(params[:event_id]), :notice => "You've been successfully reserved to the event!"
+      redirect_to event_path(@event.id), :notice => "You've been successfully reserved to the event!"
     else
-      redirect_to event_path(params[:event_id]),
+      redirect_to event_path(@event.id),
                   :alert => "Reservation could not be created because #{@reservation.errors.full_messages}"
     end
   end
@@ -31,5 +32,16 @@ class ReservationsController < ApplicationController
     @reservation.destroy
 
     redirect_to root_url, :notice => 'Your reservation has been canceled.'
+  end
+
+  private
+
+  def check_if_event_exists_and_set_event
+    return unless params[:event_id]
+
+    @event = Event.find(params[:event_id])
+
+    #TODO: Change url path to root_url once made
+    redirect_to user_path(1), :alert => 'Event does not exist.' if @event.nil?
   end
 end
