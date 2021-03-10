6d1ccb3ae433f6d78219651e87e71b13196dd16e
OccassionalDev <ericaschimmel01@gmail.com>
2021-03-10 18:48:10 -0500

    rubocop fixes

--- app/helpers/events_helper.rb
+++ app/helpers/events_helper.rb
@@ -1,9 +1,9 @@
 module EventsHelper
-    def time_range(start_datetime, end_datetime)
-        if start_datetime.to_date == end_datetime.to_date
-            "#{start_datetime.strftime('%A, %B %e, %Y %l:%M')} to #{end_datetime.strftime('%l:%M %Z')}"
-        else
-            "#{start_datetime.strftime('%A, %B %e, %Y %l:%M')} to #{end_datetime.strftime('%A, %B %e, %Y %l:%M %Z')}"
-        end
+  def time_range(start_datetime, end_datetime)
+    if start_datetime.to_date == end_datetime.to_date
+      "#{start_datetime.strftime('%A, %B %e, %Y %l:%M')} to #{end_datetime.strftime('%l:%M %Z')}"
+    else
+      "#{start_datetime.strftime('%A, %B %e, %Y %l:%M')} to #{end_datetime.strftime('%A, %B %e, %Y %l:%M %Z')}"
     end
+  end
 end
