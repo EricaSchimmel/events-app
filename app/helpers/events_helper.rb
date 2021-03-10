da6e19fe3954fc0c348d4720d05df978930e1429
OccassionalDev <ericaschimmel01@gmail.com>
2021-03-10 18:50:50 -0500

    added a format_start_time helper to Events helper

--- app/helpers/events_helper.rb
+++ app/helpers/events_helper.rb
@@ -6,4 +6,8 @@ module EventsHelper
       "#{start_datetime.strftime('%A, %B %e, %Y %l:%M')} to #{end_datetime.strftime('%A, %B %e, %Y %l:%M %Z')}"
     end
   end
+
+  def format_start_time(start_datetime)
+    start_datetime.strftime('%A, %B %e, %Y %l:%M').to_s
+  end
 end
