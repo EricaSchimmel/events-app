0ab51039295ca05e9aac9e831ca77a2b8211fe96
OccassionalDev <ericaschimmel01@gmail.com>
2021-03-10 19:04:08 -0500

    added a helper to Reservations helper for checking if the current user is reserved to a event

--- app/helpers/reservations_helper.rb
+++ app/helpers/reservations_helper.rb
@@ -1,2 +1,11 @@
 module ReservationsHelper
+  def user_reserved_to_event?(event)
+    if current_user.id == event.host_id
+      true
+    elsif event.guests.find_by(:user_id => current_user.id).nil?
+      false
+    else
+      true
+    end
+  end
 end
